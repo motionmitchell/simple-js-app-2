e10626aecca838d73b06cd691ccbae395ed23d87
motionmitchell <77647201+motionmitchell@users.noreply.github.com>
2021-03-18 21:02:52 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,3 @@
+  let pokemonList = [name: "bulbasaur", type: "grass", height: "7"] ;
+let pokemonList = [name: "Charmeleon", type: "fire", height: "1.1"]
+  let pokemonList[name: "squirtle", type: "water", height: "0.5"]
